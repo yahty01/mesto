5c0743cd9ff53b6fc7282f8db892dab95c964015
YAHTY <119876639+yahty01@users.noreply.github.com>
2023-04-13 22:25:01 +0300

    ----

---
+++ script/stockCards.js
@@ -0,0 +1,26 @@
+const initialCards = [ 
+  { 
+    name: 'Архыз', 
+    link: 'https://images.unsplash.com/photo-1681121882740-561016c015dd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80' 
+  }, 
+  { 
+    name: 'Челябинская область', 
+    link: 'https://images.unsplash.com/photo-1681121881794-d3683ede874b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80' 
+  }, 
+  { 
+    name: 'Иваново', 
+    link: 'https://images.unsplash.com/photo-1568231340347-ba25f4c0e258?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80' 
+  }, 
+  { 
+    name: 'Камчатка', 
+    link: 'https://images.unsplash.com/photo-1572986350786-002d08b6b87e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80' 
+  }, 
+  { 
+    name: 'Холмогорский район', 
+    link: 'https://images.unsplash.com/photo-1548238953-ef404ef937b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80' 
+  }, 
+  { 
+    name: 'Байкал', 
+    link: 'https://images.unsplash.com/photo-1533661537256-701c0084511d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=705&q=80' 
+  } 
+]; 
