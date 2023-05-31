0014841c7a33dead0e9e4534a8b4b3019a0a9837
YAHTY <119876639+yahty01@users.noreply.github.com>
2023-05-31 18:35:09 +0300

    -

--- utils/constants.js
+++ utils/constants.js
@@ -1,9 +1,9 @@
-export const buttonEditProfile = document.querySelector(".profile__edit-button");
 export const buttonAddCard = document.querySelector(".profile__add-button");
+export const buttonEditProfile = document.querySelector(".profile__edit-button");
 export const formProfile = document.querySelector(".form_type_profile");
 export const formCards = document.querySelector(".form_type_cards");
-export const jobInput = document.querySelector(".form__input_type_job");
 export const nameInput = document.querySelector(".form__input_type_name");
+export const jobInput = document.querySelector(".form__input_type_job");
 
 export const config = {
   formSelector: '.form',
