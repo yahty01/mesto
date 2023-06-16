3489de3b8d57c6b54b71db13d0de5eebdebcc61b
YAHTY <119876639+yahty01@users.noreply.github.com>
2023-06-16 17:17:52 +0300

    0

--- webpack.config.js
+++ webpack.config.js
@@ -4,49 +4,56 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: {
-    main: './src/pages/index.js'
-  },
+  entry: './src/pages/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-    publicPath: '',
+    filename: 'index.js',
+    publicPath: ''
   },
   mode: 'development',
   devServer: {
     static: path.resolve(__dirname, './dist'),
     open: true,
-    compress: true,
-    port: 3000
+    port: 3000,
+    compress: true
   },
+  devtool: 'eval-source-map',
   module: {
-    rules: [{
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: '/node_modules/'
-      },
-      {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, {
+    rules: [
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, {
             loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            }
+            options: { importLoaders: 1 }
           },
-          'postcss-loader'
-        ]
-      },
-    ]
+        'postcss-loader']
+        },
+        {
+           test: /\.(png|svg|jpg|jpeg|gif)$/,
+           type: 'asset/resource',
+           generator: {
+               filename: 'images/[name].[hash][ext]',
+           }
+        },
+        {
+           test: /\.(woff|woff2|eot|ttf|otf)$/i,
+           type: 'asset/resource',
+           generator: {
+               filename: 'fonts/[name].[hash][ext]',
+           }
+        },
+        {
+             test: /\.js$/,
+             use: 'babel-loader',
+             exclude: '/node_modules/'
+           }
+      ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'}),
-    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
-
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+        template: './src/index.html'
+      })
   ]
-}
+};
